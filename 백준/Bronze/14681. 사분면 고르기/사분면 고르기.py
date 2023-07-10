20aaba58eff4c99f67831239563c3c756c9d43f1
MintCat <presidentmc9898@gmail.com>
2023-07-10 16:03:17 +0900

    [Bronze V] Title: 사분면 고르기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -2,12 +2,6 @@ import sys
 x=int(sys.stdin.readline())
 y=int(sys.stdin.readline())
 if x>0:
-    if y>0:
-        print(1)
-    else:
-        print(4)
+    print(1) if y>0 else print(4)
 else:
-    if y>0:
-        print(2)
-    else:
-        print(3)
+    print(2) if y>0 else print(3)
