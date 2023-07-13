f84ac624cbad9a4aca2033e7ec1b9b49cd7e04c7
MintCat <presidentmc9898@gmail.com>
2023-07-14 04:40:08 +0900

    [Bronze II] Title: 음계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -1,7 +1,4 @@
-l=list(map(int,input().split()))
-s=set()
-for i in range(7):
-    s.add(0) if l[i]<l[i+1] else s.add(1)
-if s=={0}: print('ascending')
-elif s=={1}: print('descending')
+s=input()
+if s=='1 2 3 4 5 6 7 8': print('ascending')
+elif s=='8 7 6 5 4 3 2 1': print('descending')
 else: print('mixed')
