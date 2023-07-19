ab6dc3286129cc433b92120f77258fc61036de71
MintCat <presidentmc9898@gmail.com>
2023-07-19 10:43:15 +0900

    [Bronze II] Title: 숫자의 개수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,11 +1,4 @@
-n=1     # 곱셈의 항등원 이용
-for i in range(3):
-    n*=int(input())
-s=str(n); d={}
+# 이렇게 심플하게 풀 수 있다니..
+n=int(input())*int(input())*int(input())
 for i in range(10):
-    count=0
-    for w in s:
-        if w==chr(i+48):
-            count+=1
-    d[i]=count
-print('\n'.join(map(str,d.values())))
+    print(str(n).count(str(i)))
