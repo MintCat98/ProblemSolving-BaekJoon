82a08d91daec61644e98f984d80bf8a1d0a1c1f4
MintCat <presidentmc9898@gmail.com>
2023-07-19 15:46:16 +0900

    [Bronze I] Title: 팰린드롬수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -2,9 +2,5 @@ from sys import stdin as si
 while True:
     n=si.readline().strip('\n')
     if n=='0': break
-    nr=n[::-1]
     l=len(n); h=int(l/2)
-    if l%2==0:
-        print('yes') if n[:h]==nr[:h] else print('no')
-    else:
-        print('yes') if n[:h]==nr[:h] else print('no')
+    print('yes') if n==n[::-1] else print('no')
