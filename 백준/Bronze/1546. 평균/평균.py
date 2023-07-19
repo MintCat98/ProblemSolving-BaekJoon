921d77fdaaf29ad64b1ceca57b81e3040100a228
MintCat <presidentmc9898@gmail.com>
2023-07-19 14:55:51 +0900

    [Bronze I] Title: 평균, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,7 +1,4 @@
 from sys import stdin as si
 n=int(si.readline())
 l=list(map(int,si.readline().split()))
-m=max(l)
-for i in range(n):
-    l[i]=l[i]/m*100
-print(sum(l)/n)
+print(sum(l)/(max(l)*n)*100)
