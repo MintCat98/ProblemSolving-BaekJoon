b37a4a92da8900681ab881feb77c09e1a907b16b
MintCat <presidentmc9898@gmail.com>
2023-08-06 16:00:28 +0900

    [Bronze II] Title: 블랙잭, Time: 80 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,19 +1,11 @@
-from sys import stdin as si
-input=si.readline
+# 정렬리스트 테스트
 n,m=map(int,input().split())
-l=list(map(int,input().split()))
+l=list(map(int,input().split())); l.sort(reverse=True)
 save=0
-for i in range(n):
-    a=l[i]
-    for j in range(n):
-        if j==i: continue
-        b=l[j]
-        for k in range(n):
-            if k==i or k==j: continue
-            c=l[k]
-            s=a+b+c
-            if s>m: continue
-            elif s==m: save=s; break
-            else:
-                if s>save: save=s
+for i in range(n-2):
+    for j in range(i+1,n-1):    # 중복 방지
+        for k in range(j+1,n):
+            s=l[i]+l[j]+l[k]
+            if s<=m:
+                if s>save: save=s; break
 print(save)
