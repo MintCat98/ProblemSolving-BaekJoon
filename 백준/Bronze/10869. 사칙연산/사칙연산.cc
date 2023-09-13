bba33e9c79c68f2729dad9d152baae21a5ae4ffd
MintCat <presidentmc9898@gmail.com>
2023-09-13 10:03:58 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.cc
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -1,11 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-	int a; int b;
+	int a, b;
 	scanf("%d%d", &a, &b);
-	printf("%d\n", a + b);
-	printf("%d\n", a - b);
-	printf("%d\n", a * b);
-	printf("%d\n", a / b);
-	printf("%d", a % b);
+	printf("%d\n%d\n%d\n%d\n%d", a + b, a - b, a * b, a / b, a % b);
 }
