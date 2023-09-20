b8d98fb7563f69173c68d3c2d62596b3457dacf7
MintCat <presidentmc9898@gmail.com>
2023-09-20 11:06:14 +0900

    [Silver IV] Title: solved.ac, Time: 304 ms, Memory: 37512 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.py
+++ 백준/Silver/18110. solved．ac/solved．ac.py
@@ -7,13 +7,11 @@ input=stdin.readline
 n=int(input())
 if n==0: print(0)
 else:
-    h=[]; r=[]; s=0
+    h=[]; r=[]
+    # Heap-sort
     for _ in range(n):
         heapq.heappush(h,int(input()))
-    # Heap-sort
     for i in range(n):
         r.append(heapq.heappop(h))
     rd=round2(n*0.15)
-    for e in r[rd:n-rd]:
-        s+=e
-    print(round2(s/(n-2*rd)))
+    print(round2(sum(r[rd:n-rd])/(n-2*rd)))
