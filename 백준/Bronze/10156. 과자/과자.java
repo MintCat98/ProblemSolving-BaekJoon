b0ea456b68deee444732e5e8eb8b18552b907291
MintCat <presidentmc9898@gmail.com>
2024-01-04 13:46:49 +0900

    [Bronze IV] Title: 과자, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int res = arr[0] * arr[1] - arr[2];
+        if (res > 0) {
+            System.out.println(res);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
