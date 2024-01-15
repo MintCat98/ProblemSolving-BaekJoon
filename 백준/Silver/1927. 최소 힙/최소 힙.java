40369ca18febca57f2974f1ab8994ed7a780e5d9
MintCat <presidentmc9898@gmail.com>
2024-01-15 11:12:02 +0900

    [Silver II] Title: 최소 힙, Time: 1056 ms, Memory: 31584 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> h = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (h.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(h.remove());
+                }
+            } else {
+                h.add(x);
+            }
+        }
+    }
+}
