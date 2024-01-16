62c7423b3b497e2cf167a63a9dcbd664bb42a7d5
MintCat <presidentmc9898@gmail.com>
2024-01-16 11:31:49 +0900

    [Silver I] Title: 절댓값 힙, Time: 708 ms, Memory: 27056 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Long> hp = new PriorityQueue<>();
+        PriorityQueue<Long> hm = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            long x = Long.parseLong(br.readLine());
+            if (x == 0) {
+                if (hp.isEmpty() && hm.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    // 여기 로직 수정해야 함!
+                    long p = (hp.isEmpty()) ? 0 : hp.peek();
+                    long m = (hm.isEmpty()) ? 0 : hm.peek();
+                    if (p == 0) {
+                        System.out.println(hm.remove());
+                    } else if (m == 0) {
+                        System.out.println(hp.remove());
+                    } else if (p >= -m) {
+                        System.out.println(hm.remove());
+                    } else {
+                        System.out.println(hp.remove());
+                    }
+                }
+            } else if (x > 0) {
+                hp.add(x);
+            } else {
+                hm.add(x);
+            }
+        }
+    }
+}
